491f03a94ce77b8eaa21f6fffe1b74bfb3a87abf
Semi-Tough <1693416984@qq.com>
2022-05-03 17:29:49 +0800

    初始化窗口

---
+++ Console-FlyingChess/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Console_FlyingChess
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            #region 初始化窗口
+
+            const int wide = 50;
+            const int high = 30;
+            InitConsole(wide,high);
+
+            #endregion
+
+        }
+
+        private static void InitConsole(int wide,int high)
+        {
+            Console.CursorVisible = false;
+            Console.SetWindowSize(wide,high);
+            Console.SetBufferSize(wide,high);
+        }
+    }
+}
